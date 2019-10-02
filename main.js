bc87113baf4adcfca6080e7088dad8d923b0e7ce
Benedikt Schnatterbeck <benedikt.schnatterbeck@starleaf.com>
2019-10-02 12:05:59 +0100

    Minor changes to the styling. added a reset button.

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
+/* Pseudo Code:
+1.
+ */
+
 "use strict";
+
+const calcElements = {
+    // numbers
+    buttonOne: document.getElementById('one'),
+    buttonTwo: document.getElementById('two'),
+    buttonThree: document.getElementById('three'),
+    buttonFour: document.getElementById('four'),
+    buttonFive: document.getElementById('five'),
+    buttonSix: document.getElementById('six'),
+    buttonSeven: document.getElementById('seven'),
+    buttonEight: document.getElementById('eight'),
+    buttonNine: document.getElementById('nine'),
+    buttonZero: document.getElementById('zero'),
+
+    // Operators
+    buttonEquals: document.getElementById('equals'),
+    buttonPlus: document.getElementById('plus'),
+    buttonMinus: document.getElementById('minus'),
+    buttonDivide: document.getElementById('divide'),
+    buttonMultiply: document.getElementById('multiply'),
+
+    // Display
+    display: document.getElementById('display'),
+};
 
