f25e09736d98aded8b1fbfa9b7615295c6d81ee1
Benedikt <benedikt.schnatterbeck@gmail.com>
2019-10-12 14:28:59 +0100

    Completed the basic calculator using the Shunting yard algorithm/reverse polish notation got rid of switch statement.

--- main.js
+++ main.js
@@ -1,57 +1,89 @@
 "use strict";
 
-/* Pseudo code:
-1. when the user presses a button the type of button needs to be recognized:
-    - an event listener needs to be implemented for each of the three button classes (btn, dataChange, btn operator)
-    -
-    - if the button pressed is not DEL or AC then the number needs to be stored in a variable:
-        - string: numbers need to be added and removed from the string without any operation taking place
-        - array: past results should be displayed in an array to allow the user to see the history. This should go back at least to the previous result
-    - these variables should be stored in an object to avoid the global scope.
-2. the variable for the current operation should be shown in the display.
-
- */
-
 const calcObj = {
-    // numbers
-    one: document.getElementById('one').textContent,
-    two: document.getElementById('two'),
-    three: document.getElementById('three'),
-    four: document.getElementById('four'),
-    five: document.getElementById('five'),
-    six: document.getElementById('six'),
-    seven: document.getElementById('seven'),
-    eight: document.getElementById('eight'),
-    nine: document.getElementById('nine'),
-
-    // data change
-    AC: document.getElementById('AC'),
-    equals: document.getElementById('equals'),
-
-    // operators
-    minus: document.getElementById('minus'),
-    plus: document.getElementById('plus'),
-    multiply: document.getElementById('multiply'),
-    float: document.getElementById('float'),
-
-    // logic
-    calcUI: document.getElementById('grid'),
-    pastNumbers: [],
     numbers: [],
+    operators: [],
+
+    outputQueue: [],
+    operatorStack: [],
+
+    getDisplay: function() {
+        return document.getElementById('display');
+    },
+    updateDisplay: function (value) {
+        calcObj.getDisplay().textContent += value;
+    },
+    clearDisplay: function () {
+       calcObj.getDisplay().textContent = '';
+    }
 };
 
-calcObj.calcUI.addEventListener('click', function (event) {
-    const targetBtn = event.target;
-    // gets the number that was pressed and adds it to the first operator value
-    calcObj.firstOperator = targetBtn.textContent;
-    console.log(calcObj.firstOperator);
+calcObj.clearDisplay();
 
-    switch (targetBtn.id) {
-        case 'default':
-            calcObj.numbers.prototype.push(targetBtn.textContent);
-            console.log(targetBtn.id);
-        case 'equals':
-            console.log(targetBtn.id);
-        case 'AC':
+document.querySelectorAll('.btn').forEach(number => calcObj.numbers.push(number));
+document.querySelectorAll('.operator').forEach(operator => calcObj.operators.push(operator));
+
+calcObj.numbers.forEach(button => button.addEventListener('click', function () {
+    calcObj.updateDisplay(button.textContent);
+}));
+
+calcObj.operators.forEach(button => button.addEventListener('click', function () {
+    calcObj.updateDisplay(button.textContent);
+}));
+
+document.getElementById('del').addEventListener('click', function () {
+    calcObj.getDisplay().textContent = calcObj.getDisplay().textContent.slice(0, -1);
+});
+
+document.getElementById('AC').addEventListener('click', function () {
+    calcObj.clearDisplay();
+});
+
+document.getElementById('equals').addEventListener('click', function () {
+    //console.log(document.getElementById('display').textContent.match(/[^\d()]+|[\d.]+/g));
+    let equation = document.getElementById('display').textContent.match(/[^\d()]+|[\d.]+/g);
+    for (const element of equation) {
+        if (element === "+" || element === "-" || element === "*" || element === "/") {
+            calcObj.operatorStack.push(element);
+        } else {
+            calcObj.outputQueue.push(element);
+        }
     }
+    //console.log(calcObj.operatorStack);
+    //console.log(calcObj.outputQueue);
+    const stackLength = calcObj.operatorStack.length;
+
+    for (let i = 0; i < stackLength; i++) {
+        calcObj.outputQueue.push(calcObj.operatorStack.pop());
+    }
+    //console.log(calcObj.outputQueue);
+
+    function calculate(number1, number2, operator) {
+        if (operator === "+") {
+            return number1 + number2;
+        } else if (operator === "-") {
+            return number1 - number2;
+        } else if (operator === "*") {
+            return number1 * number2;
+        } else if (operator === "/") {
+            return number1 / number2;
+        } else {
+            throw "ERROR!!!!!"
+        }
+    }
+    console.log(calcObj.outputQueue);
+    for (let i = 0; i < calcObj.outputQueue.length; i++) {
+        let element = calcObj.outputQueue[i];
+        console.log(i);
+        if (element === "+" || element === "-" || element === "*" || element === "/") {
+            let number1 = parseFloat(calcObj.outputQueue[i-2]);
+            let number2 = parseFloat(calcObj.outputQueue[i-1]);
+            let result = calculate(number1, number2, element);
+            calcObj.outputQueue.splice(i-2, 3, result);
+            console.log(calcObj.outputQueue);
+            i -= 2;
+        }
+    }
+    document.getElementById('display-two').textContent
+    document.getElementById('display').textContent = calcObj.outputQueue[0];
 });
